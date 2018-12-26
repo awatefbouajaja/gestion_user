464e3d364fe07f12afb582c6dff8bb4a7eb2cf93
Awatef <bouajaja.awatef@gmail.com>
2018-12-26 15:04:00 +0100

    commit-delete

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,22 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use FOS\UserBundle\Form\Type\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\User;
+use FOS\UserBundle\Doctrine\UserManager;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+       /*$user=$this->getUser();
+        if($user) {*/
+            // replace this example code with whatever you need
+            return $this->render('default/index.html.twig');
+        /*}else {
+            return $this->render('@FOSUser/Security/login.html.twig');
+        }*/
     }
 
     /**
@@ -26,12 +31,43 @@ class DefaultController extends Controller
     public function utilisateursAction()
     {
 
-        $user=$this->getUser()->getId();
-        if($user){
+        /*$nuser=new fos_user.user_manager();
+        $form=$this->createForm(RegistrationFormType::class,$nuser,array(
+            'action'=>$this->generateUrl('add_user')
+        ));*/
+
+
+        /*$user=$this->getUser();
+        if($user){*/
             $repository = $this->get('fos_user.user_manager');
             //$repository=$this->getDoctrine()->getRepository('User');
             $users=$repository->findUsers();
+            return $this->render('@App/utilisateurs.html.twig',array('utilisateurs'=>$users));
+        /*}else {
+            return $this->render('@FOSUser/Security/login.html.twig');
+        }*/
+
+    }
+
+    /**
+     * @Route("/deleteUser/{id}", name="delete_user")
+     */
+    public function deleteAction( Request $request, $id = null )
+    {
+        $session=$request->getSession();
+        if(!$id){
+          $session->getFlashBag()->add('error',"l'utilisateur n'existe pas");
+        }else {
+            $repository = $this->get('fos_user.user_manager');
+            $user = $repository->findUserBy(array('id'=>$id));
+            dump($user);
+            die('test');
+            $repository->remove($user);
+            $repository->flush();
+            $session->getFlashBag()->add('error',"le personne est supprimé ");
+
         }
-        return $this->render('@App/utilisateurs.html.twig',array('utilisateurs'=>$users));
+        return $this->forward('AppBundle:Default:utilisateurs');
+
     }
 }
